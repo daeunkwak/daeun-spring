7616f10822f5ca458419dd840c5b2abd1aac8c0f
daeunkwak <misodaeun3@naver.com>
2022-04-05 09:45:25 +0900

    branch test

---
+++ daeun-spring/src/main/java/daeunn/daeunspring/SecurityConfig.java
@@ -0,0 +1,8 @@
+package daeunn.daeunspring;
+ import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+ import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+ @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    // @Override
+    // public void configure(HttpSecurity security)
+}
--- daeun-spring/src/main/java/daeunn/daeunspring/controller/HelloController.java
+++ daeun-spring/src/main/java/daeunn/daeunspring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// branch test
 public class HelloController {
 
     @GetMapping("hello")
